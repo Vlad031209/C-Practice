db9d921769f4aaf97f8bf6d392fd632a5a7d0d40
Vladislav Misko <miskodv@gmail.com>
2024-12-10 20:10:37 +0400

    add Task10

--- HW3Task/Program.cs
+++ HW3Task/Program.cs
@@ -3,7 +3,7 @@ using static System.Console;
 
 Clear();
 
-int[] array = {1, 4, 6, 8, 10}; 
+int[] array = [1, 4, 6, 8, 10]; 
 
 Write("Введите start: ");
 int start = int.Parse(ReadLine());
@@ -11,18 +11,18 @@ int start = int.Parse(ReadLine());
 Write("Введите end: ");
 int end = int.Parse(ReadLine());
 
-int average = 0; 
+int total = 0; 
 int count = 0; 
 
 for (int i = 0; i <= array.Length; i++)
 {
     if (i == start || i == end || (i > start && i < end))
     {
-        average += array[i]; 
+        total += array[i]; 
         count += 1; 
     }
 }
 
-int answer = average / count; 
+int answer = total / count; 
 
 Write($"Ответ: {answer}"); 
---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+double Formula(int a, int b, int c, int d)
+{
+    return a * b / (c + d); 
+}
+
+Write(Formula(2, 3, 1, 2)); 
