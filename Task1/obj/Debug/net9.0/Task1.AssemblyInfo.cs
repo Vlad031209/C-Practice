64136f7266f4ead28b4f1f02dd8562e8c2b2d328
Vladislav Misko <miskodv@gmail.com>
2024-12-19 14:47:30 +0400

    commit

--- Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24869d18d0ba6348e82cd51f189823f8e5e40e3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d60fcce3262d5778ad3eacb7a5e52b4e3b44ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Task16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+Clear(); 
+
+void GetArray(int columns, int rows)
+{
+    int[] array = new int[columns * rows]; 
+    Random rnd  = new Random(); 
+    for(int i = 0; i < columns * rows; i++)
+    {
+        array[i] = rnd.Next(1, 10); 
+    }
+
+    int count = 0; 
+    foreach(int e in array)
+    {
+        if(count % columns == 0)
+        {
+            WriteLine(); 
+            Write($"{e} "); 
+        }
+        else
+        {
+            Write($"{e} ");
+        }
+        count++; 
+    }
+}
